259a17c997a9c1e8724971989a5c5fd420e8fc2b
rushikeshzope <rushikesh.zope@gmail.com>
2023-12-20 09:38:09 +0530

    code pushed

--- script.js
+++ script.js
@@ -1,127 +1,163 @@
-// var viewRecipeButton = document.getElementById('recipebutton');
-// var modal = document.getElementById('modal');
+var viewRecipeButton = document.getElementById('recipebutton');
+var modal = document.getElementById('modal');
 
-// viewRecipeButton.addEventListener('click', function() {
-//     fetchData(); 
-//     modal.style.display = 'block';
-// });
+viewRecipeButton.addEventListener('click', function() {
+    fetchData(); 
+    modal.style.display = 'block';
+});
 
-// var cancelButton = document.getElementById('cancelButton');
+var cancelButton = document.getElementById('cancelButton');
 
-// cancelButton.addEventListener('click', function() {
-//     modal.style.display = 'none';
-// });
+cancelButton.addEventListener('click', function() {
+    modal.style.display = 'none';
+});
 
 
-// const searchBar = document.getElementById('searchBar');
+async function fetchData() {
+    try {
+        const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+        const data = await response.json();
 
-// searchBar.addEventListener('input', function() {
-//     const searchTerm = searchBar.value.trim();
+        const container = document.getElementById('containerRandomItems');
+        const h1Element = container.querySelector('h1');
+        const imgElement = container.querySelector('img');
+        h1Element.textContent = data.meals[0].strMeal;
+        imgElement.src = data.meals[0].strMealThumb;
+        imgElement.alt = data.meals[0].strMeal;
 
-//     if (searchTerm !== '') {
-//         fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`)
-//             .then(response => response.json())
-//             .then(data => {
-//                 const searchedSection = document.getElementById('searchedSection');
-//                 searchedSection.innerHTML = ''; 
+        const modalTitle = document.getElementById('foodNameModal');
+        const modalImg = document.getElementById('foodModal');
+        const ingredientList = document.getElementById('list');
 
-//                 data.meals.forEach(meal => {
-//                     const container = document.createElement('div');
-//                     container.classList.add('container');
+        modalTitle.textContent = data.meals[0].strMeal;
+        modalImg.src = data.meals[0].strMealThumb;
+        modalImg.alt = data.meals[0].strMeal;
+        ingredientList.innerHTML = '';
 
-//                     const img = document.createElement('img');
-//                     img.src = meal.strMealThumb;
-//                     img.alt = meal.strMeal;
-//                     img.classList.add('foodSearched');
+        for (let i = 1; i <= 20; i++) {
+            const ingredient = data.meals[0]['strIngredient' + i];
+            const measure = data.meals[0]['strMeasure' + i];
+            if (ingredient && measure) {
+                const listItem = document.createElement('li');
+                listItem.textContent = `${measure} ${ingredient}`;
+                ingredientList.appendChild(listItem);
+            }
+        }
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
 
-//                     const p = document.createElement('p');
-//                     p.textContent = meal.strMeal;
-//                     p.classList.add('foodnameSearched');
+window.addEventListener('load', fetchData);
 
-//                     container.appendChild(img);
-//                     container.appendChild(p);
-//                     searchedSection.appendChild(container);
-//                 });
-//             })
-//             .catch(error => console.error('Error fetching searched data:', error));
-//     }
-// });
+const searchBar = document.getElementById('searchBar');
 
-// async function fetchData(url) {
-//     try {
-//         const response = await fetch(url);
-//         const data = await response.json();
+searchBar.addEventListener('input', function() {
+    const searchTerm = searchBar.value.trim();
 
-//         const container = document.getElementById('containerRandomItems');
-//         const h1Element = container.querySelector('h1');
-//         const imgElement = container.querySelector('img');
-//         h1Element.textContent = data.meals[0].strMeal;
-//         imgElement.src = data.meals[0].strMealThumb;
-//         imgElement.alt = data.meals[0].strMeal;
+    if (searchTerm !== '') {
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`)
+            .then(response => response.json())
+            .then(data => {
+                const searchedSection = document.getElementById('searchedSection');
+                searchedSection.innerHTML = ''; 
 
-//         const modalTitle = document.getElementById('foodNameModal');
-//         const modalImg = document.getElementById('foodModal');
-//         const ingredientList = document.getElementById('list');
+                data.meals.forEach(meal => {
+                    const container = document.createElement('div');
+                    container.classList.add('container');
 
-//         modalTitle.textContent = data.meals[0].strMeal;
-//         modalImg.src = data.meals[0].strMealThumb;
-//         modalImg.alt = data.meals[0].strMeal;
+                    const img = document.createElement('img');
+                    img.src = meal.strMealThumb;
+                    img.alt = meal.strMeal;
+                    img.classList.add('foodSearched');
 
-//         ingredientList.innerHTML = '';
+                    const p = document.createElement('p');
+                    p.textContent = meal.strMeal;
+                    p.classList.add('foodnameSearched');
 
-//         for (let i = 1; i <= 20; i++) {
-//             const ingredient = data.meals[0]['strIngredient' + i];
-//             const measure = data.meals[0]['strMeasure' + i];
-//             if (ingredient && measure) {
-//                 const listItem = document.createElement('li');
-//                 listItem.textContent = `${measure} ${ingredient}`;
-//                 ingredientList.appendChild(listItem);
-//             }
-//         }
-//     } catch (error) {
-//         console.error('Error fetching data:', error);
-//     }
-// }
+                    container.appendChild(img);
+                    container.appendChild(p);
+                    searchedSection.appendChild(container);
+                });
+            })
+            .catch(error => console.error('Error fetching searched data:', error));
+    }
+});
 
-// window.addEventListener('load', function() {
-//     fetchData('https://www.themealdb.com/api/json/v1/1/random.php');
-// });
+async function fetchData(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
 
-// searchBar.addEventListener('input', function() {
-//     const searchTerm = searchBar.value.trim();
+        const container = document.getElementById('containerRandomItems');
+        const h1Element = container.querySelector('h1');
+        const imgElement = container.querySelector('img');
+        h1Element.textContent = data.meals[0].strMeal;
+        imgElement.src = data.meals[0].strMealThumb;
+        imgElement.alt = data.meals[0].strMeal;
 
-//     if (searchTerm !== '') {
-//         fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`)
-//             .then(response => response.json())
-//             .then(data => {
-//                 const searchedSection = document.getElementById('searchedSection');
-//                 searchedSection.innerHTML = ''; 
+        const modalTitle = document.getElementById('foodNameModal');
+        const modalImg = document.getElementById('foodModal');
+        const ingredientList = document.getElementById('list');
 
-//                 data.meals.forEach(meal => {
-//                     const container = document.createElement('div');
-//                     container.classList.add('container');
+        modalTitle.textContent = data.meals[0].strMeal;
+        modalImg.src = data.meals[0].strMealThumb;
+        modalImg.alt = data.meals[0].strMeal;
 
-//                     const img = document.createElement('img');
-//                     img.src = meal.strMealThumb;
-//                     img.alt = meal.strMeal;
-//                     img.classList.add('foodSearched');
+        ingredientList.innerHTML = '';
 
-//                     const p = document.createElement('p');
-//                     p.textContent = meal.strMeal;
-//                     p.classList.add('foodnameSearched');
+        for (let i = 1; i <= 20; i++) {
+            const ingredient = data.meals[0]['strIngredient' + i];
+            const measure = data.meals[0]['strMeasure' + i];
+            if (ingredient && measure) {
+                const listItem = document.createElement('li');
+                listItem.textContent = `${measure} ${ingredient}`;
+                ingredientList.appendChild(listItem);
+            }
+        }
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
 
-//                     container.appendChild(img);
-//                     container.appendChild(p);
-//                     searchedSection.appendChild(container);
-//                 });
+window.addEventListener('load', function() {
+    fetchData('https://www.themealdb.com/api/json/v1/1/random.php');
+});
 
-//                 const hiddenSection = document.getElementById('hiddenSection');
-//                 hiddenSection.style.display = 'block';
-//             })
-//             .catch(error => console.error('Error fetching searched data:', error));
-//     } else {
-//         const hiddenSection = document.getElementById('hiddenSection');
-//         hiddenSection.style.display = 'none';
-//     }
-// });
+searchBar.addEventListener('input', function() {
+    const searchTerm = searchBar.value.trim();
 
+    if (searchTerm !== '') {
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`)
+            .then(response => response.json())
+            .then(data => {
+                const searchedSection = document.getElementById('searchedSection');
+                searchedSection.innerHTML = ''; 
+
+                data.meals.forEach(meal => {
+                    const container = document.createElement('div');
+                    container.classList.add('container');
+
+                    const img = document.createElement('img');
+                    img.src = meal.strMealThumb;
+                    img.alt = meal.strMeal;
+                    img.classList.add('foodSearched');
+
+                    const p = document.createElement('p');
+                    p.textContent = meal.strMeal;
+                    p.classList.add('foodnameSearched');
+
+                    container.appendChild(img);
+                    container.appendChild(p);
+                    searchedSection.appendChild(container);
+                });
+
+                const hiddenSection = document.getElementById('hiddenSection');
+                hiddenSection.style.display = 'block';
+            })
+            .catch(error => console.error('Error fetching searched data:', error));
+    } else {
+        const hiddenSection = document.getElementById('hiddenSection');
+        hiddenSection.style.display = 'none';
+    }
+});
