ea46bb9b9addb5c0b8e4ba194527fa5d9afb692b
rushikeshzope <rushikesh.zope@gmail.com>
2023-12-20 09:46:29 +0530

    final code push

--- script.js
+++ script.js
@@ -1,88 +1,17 @@
 var viewRecipeButton = document.getElementById('recipebutton');
 var modal = document.getElementById('modal');
+var cancelButton = document.getElementById('cancelButton');
+const searchBar = document.getElementById('searchBar');
 
 viewRecipeButton.addEventListener('click', function() {
     fetchData(); 
     modal.style.display = 'block';
 });
 
-var cancelButton = document.getElementById('cancelButton');
-
 cancelButton.addEventListener('click', function() {
     modal.style.display = 'none';
 });
 
-
-async function fetchData() {
-    try {
-        const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
-        const data = await response.json();
-
-        const container = document.getElementById('containerRandomItems');
-        const h1Element = container.querySelector('h1');
-        const imgElement = container.querySelector('img');
-        h1Element.textContent = data.meals[0].strMeal;
-        imgElement.src = data.meals[0].strMealThumb;
-        imgElement.alt = data.meals[0].strMeal;
-
-        const modalTitle = document.getElementById('foodNameModal');
-        const modalImg = document.getElementById('foodModal');
-        const ingredientList = document.getElementById('list');
-
-        modalTitle.textContent = data.meals[0].strMeal;
-        modalImg.src = data.meals[0].strMealThumb;
-        modalImg.alt = data.meals[0].strMeal;
-        ingredientList.innerHTML = '';
-
-        for (let i = 1; i <= 20; i++) {
-            const ingredient = data.meals[0]['strIngredient' + i];
-            const measure = data.meals[0]['strMeasure' + i];
-            if (ingredient && measure) {
-                const listItem = document.createElement('li');
-                listItem.textContent = `${measure} ${ingredient}`;
-                ingredientList.appendChild(listItem);
-            }
-        }
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
-}
-
-window.addEventListener('load', fetchData);
-
-const searchBar = document.getElementById('searchBar');
-
-searchBar.addEventListener('input', function() {
-    const searchTerm = searchBar.value.trim();
-
-    if (searchTerm !== '') {
-        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`)
-            .then(response => response.json())
-            .then(data => {
-                const searchedSection = document.getElementById('searchedSection');
-                searchedSection.innerHTML = ''; 
-
-                data.meals.forEach(meal => {
-                    const container = document.createElement('div');
-                    container.classList.add('container');
-
-                    const img = document.createElement('img');
-                    img.src = meal.strMealThumb;
-                    img.alt = meal.strMeal;
-                    img.classList.add('foodSearched');
-
-                    const p = document.createElement('p');
-                    p.textContent = meal.strMeal;
-                    p.classList.add('foodnameSearched');
-
-                    container.appendChild(img);
-                    container.appendChild(p);
-                    searchedSection.appendChild(container);
-                });
-            })
-            .catch(error => console.error('Error fetching searched data:', error));
-    }
-});
 
 async function fetchData(url) {
     try {
